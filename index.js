1ec95268a0b58c7f4636ce8c0a5388b19c0ecf1b
Stephen <smjarr89@gmail.com>
2018-06-18 13:40:28 -0400

    added the id template for each contact and navigation on each page

--- index.js
+++ index.js
@@ -29,15 +29,23 @@ app.get('/contacts', (req,res) => {
 //Contacts detail page: show the user all info for one contact
 app.get('/contacts/:id', (req, res) => {
     // res.send(`You are viewing details for ${req.params.id}.`)
-    let theID = req.params.id;
-    let contact = contacts.users.find((user) => {
-        return user.id === theID
-    });
-
+    // let theID = req.params.id;
+    let theFirstName = req.params.id;
     //Check if 'contact is valid
     //is it undefined or a real object?
+    let contact = contacts.users.find((user) => {
+        return user.firstName === theFirstName;
+    }
+);
+    res.render('id', {
+        // contactArray: contact.elements
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        phone: contact.phone,
+        email: contact.email
+    });
 
-    res.send(contact);
+    // res.send(contact);
 })
 
 
